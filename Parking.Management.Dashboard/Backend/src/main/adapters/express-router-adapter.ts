fa7cacca5951f883dd6664b574b2a32c975d3971
Dev <dev@code.com>
2023-07-13 23:04:54 -0300

    Add: AdaptRoute

---
+++ Parking.Management.Dashboard/Backend/src/main/adapters/express-router-adapter.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export const adaptRoute = (controller: any) => {
+  return async (request: Request, response: Response) => {
+    const httpResponse = await controller.handle(request);
+    response.status(httpResponse.statusCode).json(httpResponse.data);
+  };
+};
