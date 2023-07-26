8c9439ccae3b30b655ea483eb94f1acdf0683624
Dev <dev@code.com>
2023-07-25 23:22:32 -0300

    Add: New interfaces in college

--- Parking.Management.Dashboard/Backend/src/domain/contracts/prisma/college-repository.ts
+++ Parking.Management.Dashboard/Backend/src/domain/contracts/prisma/college-repository.ts
@@ -2,4 +2,7 @@ import { College } from "../../entities";
 
 export interface CollegeRepository {
   create(college: College): Promise<College>;
+  update(college: College): Promise<College>;
+  delete(id: string): Promise<College>;
+  findById(id: string): Promise<College>;
 }
