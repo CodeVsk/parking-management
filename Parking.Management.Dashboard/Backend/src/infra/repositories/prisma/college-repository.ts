74ad35c0733fbd2cd55a5861b88f7561129a9277
Dev <dev@code.com>
2023-07-25 23:25:11 -0300

    Add: College repository (Update, Delete, FindById)

--- Parking.Management.Dashboard/Backend/src/infra/repositories/prisma/college-repository.ts
+++ Parking.Management.Dashboard/Backend/src/infra/repositories/prisma/college-repository.ts
@@ -4,10 +4,42 @@ import { prisma } from "../../database/Prisma";
 
 export class PrismaCollegeRepository implements CollegeRepository {
   async create(college: College): Promise<College> {
-    console.table(college);
     const result = await prisma.college.create({
       data: {
         ...college,
+      },
+    });
+
+    return result;
+  }
+
+  async update(college: College): Promise<College> {
+    const result = await prisma.college.update({
+      where: {
+        id: college.id,
+      },
+      data: {
+        ...college,
+      },
+    });
+
+    return result;
+  }
+
+  async delete(id: string): Promise<College> {
+    const result = await prisma.college.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    return result;
+  }
+
+  async findById(id: string): Promise<College> {
+    const result = await prisma.college.findUnique({
+      where: {
+        id: id,
       },
     });
 
