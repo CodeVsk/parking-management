99174702dad548e6fc236e6b510e15503065e751
Dev <dev@code.com>
2023-08-08 00:17:36 -0300

    fix: files import

--- Parking.Management.Dashboard/Backend/src/presentation/router/routes/college.ts
+++ Parking.Management.Dashboard/Backend/src/presentation/router/routes/college.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { adaptRoute } from "../../../infra/adapters/express-router-adapter";
-import { createCollegeController } from "../../../domain/use-cases/college/create";
-import { updateCollegeController } from "../../../domain/use-cases/college/update";
-import { findByIdCollegeController } from "../../../domain/use-cases/college/find-by-id";
-import { deleteCollegeController } from "../../../domain/use-cases/college/delete";
+import { adaptRoute } from "@/infra/adapters/express-router-adapter";
+import { createCollegeController } from "@/domain/use-cases/college/create";
+import { updateCollegeController } from "@/domain/use-cases/college/update";
+import { findByIdCollegeController } from "@/domain/use-cases/college/find-by-id";
+import { deleteCollegeController } from "@/domain/use-cases/college/delete";
 
 export default (router: Router): void => {
   router.get("/college/:id", adaptRoute(findByIdCollegeController));
--- Parking.Management.Dashboard/Backend/src/presentation/server.ts
+++ Parking.Management.Dashboard/Backend/src/presentation/server.ts
@@ -1,4 +1,4 @@
-import { app, env } from "../infra/config";
+import { app, env } from "@/infra/config";
 
 const { port } = env;
 
