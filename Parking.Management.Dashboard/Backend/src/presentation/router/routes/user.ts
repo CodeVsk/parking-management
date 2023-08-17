f2b528d694ac178f9900e64591214c81d6a5a822
CodeVsk <zimaplay1@gmail.com>
2023-08-17 00:42:01 -0300

    feat: register user &  validation method in utils fix: return json in adaptRoute.ts fix: cotent object type in resut.ts

--- Parking.Management.Dashboard/Backend/src/presentation/router/routes/user.ts
+++ Parking.Management.Dashboard/Backend/src/presentation/router/routes/user.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { adaptRoute } from "../../../infra/adapters/express-router-adapter";
-//import { authMiddleware } from "@/infra/factory/auth-factory";
+import { authMiddleware } from "@/infra/factory/auth-factory";
 import { deleteUserController } from "@/domain/use-cases/user/delete";
 import { createUserController } from "@/domain/use-cases/user/create";
 import { updateUserController } from "@/domain/use-cases/user/update";
@@ -9,15 +9,11 @@ import { AuthProvider } from "@/infra/providers";
 import { AuthMiddleware } from "@/infra/http/middleware/auth-middleware";
 
 export default (router: Router): void => {
-  const provider = new AuthProvider();
-  const authMiddleware = new AuthMiddleware(provider);
-
-  router.get("/user/:id", adaptRoute(findByIdUserController));
-  router.put("/user", adaptRoute(updateUserController));
-  router.post(
-    "/user",
-    //(req, res, next) => authMiddleware.execute(req, res, next),
-    adaptRoute(createUserController)
+  router.get(
+    "/user/:id",
+    (req, res, next) => authMiddleware.isLoggedAdmin(req, res, next),
+    adaptRoute(findByIdUserController)
   );
+  router.put("/user", adaptRoute(updateUserController));
   router.delete("/user/:id", adaptRoute(deleteUserController));
 };
