9bc6fb6c409e7d5248735adf0172bb17ae6ba771
CodeVsk <zimaplay1@gmail.com>
2023-08-27 19:52:26 -0300

    feat: AutoMap decorator in entities & dto class

--- Parking.Management.Dashboard/Backend/src/application/dtos/garage-dto.ts
+++ Parking.Management.Dashboard/Backend/src/application/dtos/garage-dto.ts
@@ -1,12 +1,20 @@
+import { AutoMap } from "@automapper/classes";
 import { VehicleStatus } from "../../domain/enums/vehicle-status";
 import { EntityDto } from "./entity-dto";
 
 export class GarageDto extends EntityDto {
+  @AutoMap()
   status: VehicleStatus;
+  @AutoMap()
   entryTime: Date;
+  @AutoMap()
   departureTime: Date;
+  @AutoMap()
   collegeId: string;
+  @AutoMap()
   vehicleId: string;
+  @AutoMap()
   userEntryId: string;
+  @AutoMap()
   userDepartureId: string;
 }
--- Parking.Management.Dashboard/Backend/src/domain/entities/vehicle-note.ts
+++ Parking.Management.Dashboard/Backend/src/domain/entities/vehicle-note.ts
@@ -1,8 +1,13 @@
+import { AutoMap } from "@automapper/classes";
 import { Entity } from "../../core/domain/entity";
 
 export class VehicleNote extends Entity {
+  @AutoMap()
   description: string;
+  @AutoMap()
   vehicleId: string;
+  @AutoMap()
   created_at?: Date;
+  @AutoMap()
   updated_at?: Date;
 }
