e018e63c419e10de04c0e5263ca0390da1d011d0
CodeVsk <zimaplay1@gmail.com>
2023-11-13 00:07:38 -0300

    fix: remove compose network fix: app server cors to general remove: uselles code history

--- Parking.Management.Dashboard/Backend/src/infra/config/app.ts
+++ Parking.Management.Dashboard/Backend/src/infra/config/app.ts
@@ -8,7 +8,7 @@ const app = express();
 mapper.createMapper();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 
 setupRoutes(app);
 
